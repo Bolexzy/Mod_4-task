9976a48b25d5c9cb118b277b455d9d68f6938283
Bolexzy <boluoyewumi79@gmail.com>
2022-09-10 15:40:54 +0100

    Finish mod_4 assignment

--- mod4_solution/easier/script.js
+++ mod4_solution/easier/script.js
@@ -43,6 +43,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // See Lecture 50, part 1
 for (var i = 0; i < names.length; i++) {
 
+  
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
@@ -51,17 +52,17 @@ for (var i = 0; i < names.length; i++) {
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
   var firstLetter = names[i].charAt(0).toLowerCase();
-
+  
   // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
+    console.log(byeSpeaker.speak(names[i]));
   } else {
-    helloSpeaker.speak(names[i]);
-  }
+    console.log(helloSpeaker.speak(names[i]));
+  } 
 }
 
 })();
