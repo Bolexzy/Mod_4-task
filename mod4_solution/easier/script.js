df546bc79107186b7f424a4062fd812f0aa935c5
Bolexzy <boluoyewumi79@gmail.com>
2022-09-10 15:57:56 +0100

    Finish mod_4 assignment

--- mod4_solution/easier/script.js
+++ mod4_solution/easier/script.js
@@ -59,9 +59,9 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter === 'j') {
-    console.log(byeSpeaker.speak(names[i]));
+     console.log(byeSpeaker.speak(names[i]));
   } else {
-    console.log(helloSpeaker.speak(names[i]));
+     console.log(helloSpeaker.speak(names[i]));
   } 
 }
 
